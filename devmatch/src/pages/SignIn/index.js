ddb45b8e56126a22bbfeb5b54f16fb12c45f349c
Everton santos <everton0102@gmail.com>
2021-09-21 17:30:25 -0300

    Tela signIn pronta

--- devmatch/src/pages/SignIn/index.js
+++ devmatch/src/pages/SignIn/index.js
@@ -1,8 +1,34 @@
-
+import './styles.css'
+import '../../styles/form.css'
+import {Link} from 'react-router-dom'
 
 function SignIn() {
     return (
-        <h1>signIn</h1>
+        <div className = "container-form">
+            <form className=" form form-sign-in">
+               <div className= "text-center mb-lg">
+                   <h1>Entrar</h1>
+                   <Link to="/sing-up">Cadastre-se</Link>
+                </div>
+                <div>
+                   <div >
+                       <label htmlFor="email">E-mail</label>
+                       <input id="email" type="text" placeholder="Digite seu e-mail"/>
+                   </div>
+                   <div >
+                       <label htmlFor="password">Senha</label>
+                       <input id="password" type="password" placeholder="Digite sua senha"/>
+                   </div>
+                   <button className="btn-dark-blue">Entrar</button>
+                   <div className="flex-row items-center">
+                      <input type="checkbox" value="Lembrar-me" name="remember"/> 
+                      <span htmlFor="remember">Lembrar-me</span>
+                   </div>
+                </div>
+                <Link to="/recovery-password" className="my-lg">Esqueceu sua senha?</Link> 
+                <span className="light-label">@2021 Todos os Direitos Reservados</span>
+            </form>
+        </div>
     )
 
 }
